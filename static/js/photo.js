fa0fe7d26117bfdc448f306dfe290d73758bc737
MikhaelMIEM <mvnikolenko@edu.hse.ru>
2020-12-03 12:14:53 +0300

    add photo download

--- static/js/photo.js
+++ static/js/photo.js
@@ -57,14 +57,10 @@
   }
 
   function download(filename, data) { 
-    //creating an invisible element 
     var element = document.createElement('a'); 
     element.setAttribute('href',  data); 
     element.setAttribute('download', filename); 
-    // Above code is equivalent to 
-    // <a href="path of file" download="file name"> 
     document.body.appendChild(element); 
-    //onClick property 
     element.click(); 
     document.body.removeChild(element); 
   } 
