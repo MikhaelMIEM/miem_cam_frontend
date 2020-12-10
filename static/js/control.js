f26c0780597a0bbc4eeeac567b429dd8cf3a2101
MikhaelMIEM <mvnikolenko@edu.hse.ru>
2020-12-10 11:22:12 +0300

    add camera control

---
+++ static/js/control.js
@@ -0,0 +1,180 @@
+var controlserver = "http://19111.miem.vmnet.top";
+var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0XzFAZW1haWwuY29tIiwiZXhwIjoxNjA4MTQyMjkwfQ.1K7A7wS8UaXleXIbH3TX8zmWe1s5oyUMZgjPk6stNY4";
+
+function up() {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[0,0.1,0]});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));  
+
+};
+
+
+function down() {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[0,-0.1,0]});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+};
+
+
+function left() {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[-0.1,0,0]});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+};
+
+
+function right() {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[0.1,0,0]});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+
+}
+
+
+function zoomin() {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[0,0,0.1]});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+
+};
+
+
+function zoomout() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var raw = JSON.stringify({"ptz":[0,0,-0.1]});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(controlserver + "/continuous_move", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+};
+
+
+function camstop() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    fetch(controlserver + "/stop", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+};
+
+function chosecam(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("key", key);
+    
+    var raw = JSON.stringify({"uid": document.getElementById('cams').value.toString()
+    ,"date_on":"2020-12-10T08:05:29.296Z",
+    "date_off":"2020-12-10T08:05:29.296Z"});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(controlserver + "/chose_cam", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+};
